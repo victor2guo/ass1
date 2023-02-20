603a5a6ec76c78ea6456a9bafb3ec803a2067578
ziyang <ziyangguo222@gmail.com>
2023-02-20 19:02:47 +0800

    updates for output info

--- clean.py
+++ clean.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import argparse
 
 df1 = pd.read_csv('respondent_contact.csv')
 df2 = pd.read_csv('respondent_other.csv')
@@ -12,3 +14,12 @@ data = data[~data['job'].str.contains('insurance')]
 data = data[~data['job'].str.contains('Insurance')]
 
 data.to_csv('output_file.csv')
+
+parser = argparse.ArgumentParser(description='Ass1')
+parser.add_argument('respondent_contact.csv', type=str, help='contact_info_file')
+parser.add_argument('respondent_other.csv', type=str, help='other_info_file')
+parser.add_argument('output_file.csv', type=str, help='output_file')
+
+args = parser.parse_args()
+
+print(args.file1, args.file2, args.file3)
