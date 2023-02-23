b70e6daad152e72731e285ae6390f12352a5b1b1
ziyang <ziyangguo222@gmail.com>
2023-02-23 15:05:15 +0800

    cleaned data

--- clean.py
+++ clean.py
@@ -1,25 +1,21 @@
 import pandas as pd
-import numpy as np
-import argparse
+def clean(input_file1,input_file2):
+    df1 = pd.read_csv(input_file1)
+    df2 = pd.read_csv(input_file2)
+    df = pd.merge(df1, df2, left_on="respondent_id", right_on="id").drop('id', axis=1)
+    df.dropna(inplace=True)
+    df = df[df['job'].str.contains('insurance') == False]
+    df = df[df['job'].str.contains('Insurance') == False]
+    return df
 
-df1 = pd.read_csv('respondent_contact.csv')
-df2 = pd.read_csv('respondent_other.csv')
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Clean respondent data')
+    parser.add_argument('contact_info_file', type=str, help='respondent_contact.csv')
+    parser.add_argument('other_info_file', type=str, help='respondent_other.csv')
+    parser.add_argument('output_file', type=str, help='output_file.csv')
+    args = parser.parse_args()
 
-df1.rename(columns = {'respondent_id':'id'},inplace = True)
-data = pd.merge(df1,df2,on = 'id')
-
-data.dropna(inplace = True)
-
-data = data[~data['job'].str.contains('insurance')]
-data = data[~data['job'].str.contains('Insurance')]
-
-data.to_csv('output_file.csv')
-
-parser = argparse.ArgumentParser(description='Ass1')
-parser.add_argument('respondent_contact.csv', type=str, help='contact_info_file')
-parser.add_argument('respondent_other.csv', type=str, help='other_info_file')
-parser.add_argument('output_file.csv', type=str, help='output_file')
-
-args = parser.parse_args()
-
-print(args.file1, args.file2, args.file3)
+cleaned = clean(args.contact_info_file, args.other_info_file)
+print('The shape of out file is: ', cleaned.shape)
+cleaned.to_csv(args.output, index=False)
